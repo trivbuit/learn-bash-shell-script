8d97f7507acdbf712f3388f44721e86b8b891d4f
root <root@localhost.localdomain>
2021-04-02 04:30:27 -0400

    Add script install ohmyzsh

---
+++ install_ohmyzsh.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+# OS_WHICH=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+OS_WHICH=$(. /etc/os-release; echo "$NAME")
+OS_CENTOS="CentOS Linux"
+OS_UBUNTU="Ubuntu"
+
+function install_zsh_centos { 
+    sudo -i
+    yum install zsh -y
+}
+function install_zsh_ubuntu { 
+    sudo -i
+    apt install zsh -y
+}
+
+if [[ $OS_WHICH = $OS_CENTOS ]]; then
+    echo "You are on $OS_CENTOS"
+    install_zsh_centos
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "You are on $OS_UBUNTU"
+    install_zsh_ubuntu
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
